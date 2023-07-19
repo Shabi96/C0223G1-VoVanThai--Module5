b3033e7497d3434d68f8d4b9006ccc32ef4da9d8
Shabi96 <vuvivokiller@gmail.com>
2023-07-19 15:41:28 +0700

    fix comment ngày 19/7

---
+++ ss2_react_js_and_jsx/bai_tap/bai_tap_1/element-student/src/index.js
@@ -0,0 +1,67 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+const students = [
+  {
+    id: 1,
+    company: 'Alfreds Futterkiste',
+    contact: 'Maria Anders',
+    country: 'Germany'
+  },
+  {
+    id: 2,
+    company: 'Centro comercial Moctezuma',
+    contact: 'Francisco Chang',
+    country: 'Mexico'
+  },
+  {
+    id: 3,
+    company: 'Ernst Handel',
+    contact: 'Roland Mendel',
+    country: 'Austria'
+  },
+  {
+    id: 4,
+    company: 'Island Trading',
+    contact: 'Helen Bennett',
+    country: 'UK'
+  },
+  {
+    id: 5,
+    company: 'Laughing Bacchus Winecellars',
+    contact: 'Yoshi Tannamuri',
+    country: 'Canada'
+  },
+  {
+    id: 5,
+    company: 'Magazzini Alimentari Riuniti',
+    contact: 'Giovanni Rovelli',
+    country: 'Italy'
+  }
+];
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <table>
+    <caption><h2>Students</h2></caption>
+    <thead>
+      <tr>
+        <th>Company</th>
+        <th>Contact</th>
+        <th>Country</th>
+      </tr>
+    </thead>
+    <tbody>
+      {
+        students.map(student => (
+          <tr key={student.id}>
+            <td>{student.company}</td>
+            <td>{student.contact}</td>
+            <td>{student.country}</td>
+          </tr>
+        ))
+      }
+    </tbody>
+  </table>
+);
+
